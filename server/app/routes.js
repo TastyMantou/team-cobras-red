dc8cacb1d59ddd25a3c18c75b4a973fbc5b8ec92
Kevin Shreve <kshreve@gmail.com>
2016-06-26 16:09:43 -0400

    Hello Vue View

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,65 +1,71 @@
-'use strict'
+'use strict';
 
-module.exports = function(app, passport) {
+module.exports = function (app, passport) {
 
-	/******************************normal routes***************************************/
-	// show home page
-	app.get('/', function(req, res) {
-		res.render('login.ejs', { message: req.flash('loginMessage') })
-	})
-	// show profile page
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user
-		})
-	})
-	// logout page
-	app.get('/logout', function(req, res) {
-		req.logout()
-		res.render('login.ejs', { message: req.flash('loginMessage') })
-	})
+    /******************************normal routes***************************************/
+    // show home page
+    app.get('/', function (req, res) {
+        res.render('login.ejs', { message: req.flash('loginMessage') })
+    });
+    // show profile page
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile.ejs', {
+            user: req.user
+        })
+    });
 
-	/*******************************local authentication****************************/
-	// show login page
-	app.get('/login', function(req, res) {
-		res.render('login.ejs', { message: req.flash('loginMessage') })
-	})
+    app.get('/tasks', isLoggedIn, function (req, res) {
+        res.render('tasks.ejs', {
+            user: req.user
+        })
+    });
 
-	// process a login
-	app.post('/login', passport.authenticate('local-login', {
-		successRedirect : '/profile', // if success redirect to profile page
-		failureRedirect : '/login', // if failure rediect to login page
-		failureFlash : true // allow flash messages
-	}))
+    // logout page
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.render('login.ejs', { message: req.flash('loginMessage') })
+    });
 
-	/******show sign up page*****/
-	app.get('/signup', function(req, res) {
-		res.render('signup.ejs', { message: req.flash('loginMessage') })
-	})
+    /*******************************local authentication****************************/
+    // show login page
+    app.get('/login', function (req, res) {
+        res.render('login.ejs', { message: req.flash('loginMessage') })
+    });
 
-	/******process a sign up*****/
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/profile',
-		failureRedirect : '/signup',
-		failureFlash : true
-	}))
+    // process a login
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/profile', // if success redirect to profile page
+        failureRedirect: '/login', // if failure rediect to login page
+        failureFlash:    true // allow flash messages
+    }));
 
+    /******show sign up page*****/
+    app.get('/signup', function (req, res) {
+        res.render('signup.ejs', { message: req.flash('loginMessage') })
+    });
 
+    /******process a sign up*****/
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/profile',
+        failureRedirect: '/signup',
+        failureFlash:    true
+    }));
 
-	/***************************unlink local account******************************/
-	app.get('/unlink/local', function(req, res) {
-		let user = req.user
-		user.local.email = undefined
-		user.local.password = undefined
-		user.save(function(err) {
-			res.redirect('/profile')
-		})
-	})
+    /***************************unlink local account******************************/
+    app.get('/unlink/local', function (req, res) {
+        let user = req.user;
+        user.local.email = undefined;
+        user.local.password = undefined;
+        user.save(function () {
+            res.redirect('/profile')
+        })
+    })
+};
+
+/********** check to see if user is authenticated for profile access***********/
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    } // if authenticated then go to next middleware
+    res.redirect('/'); // if not authenticated then redirect to home page
 }
-
-	/********** check to see if user is authenticated for profile access***********/
-	function isLoggedIn(req, res, next) {
-		if (req.isAuthenticated())
-			return next() // if authenticated then go to next middleware
-		res.redirect('/') // if not authenticated then redirect to home page
-	}
--- server/server.js
+++ server/server.js
@@ -1,78 +1,68 @@
-'use strict'
+'use strict';
 
-let express  = require('express'),
-	mongoose = require('mongoose'),
-	passport = require('passport'),
-	flash = require('connect-flash'),
-	http = require('http'),
-	bodyParser = require('body-parser'),
-	cookieParser = require('cookie-parser'),
-	path = require('path'),
-	session = require('express-session')
+let express = require('express'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    flash = require('connect-flash'),
+    http = require('http'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    path = require('path'),
+    session = require('express-session');
 
-let app  = express()
-const port     = process.env.PORT || 8080
-const configDB = 'mongodb://localhost/reddittaskdb'
+let app = express();
+const port = process.env.PORT || 8080;
+const configDB = 'mongodb://localhost/reddittaskdb';
 
-mongoose.connect(configDB)
+mongoose.connect(configDB);
 
-require('./config/passport')(passport) // pass passport object to auth functions
+require('./config/passport')(passport); // pass passport object to auth functions
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.use(cookieParser()); // cookies for auth
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({
+    name:              'redditTaskSessions',
+    secret:            'fghtrfhdvf',
+    resave:            false,
+    saveUninitialized: false,
+    cookie:            {
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000) // 24 hours
+    }
+}));
 
-app.use(cookieParser()) // cookies for auth
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(session({ 
-  name: 'redditTaskSessions',
-	secret: 'fghtrfhdvf',
-	resave: false,
-  saveUninitialized: false,
-  cookie: {
-	  expires: new Date(Date.now() + 24 * 60 * 60 * 1000) // 24 hours
-  }
-}))
-
-
-app.use(passport.initialize())
-app.use(passport.session()) // persistent login sessions
-app.use(flash()) // use for flash messages stored in session
-
-
-
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use for flash messages stored in session
 
 // load routes and pass in app and configured passport
-require('./app/routes.js')(app, passport)
-
-
-
-
+require('./app/routes.js')(app, passport);
 
 // create server object
-let server = http.createServer(app)
+let server = http.createServer(app);
 // booting up server function
-let boot = function() {
-	server.listen(port, function() {
-		console.log('Express server listening on port ', port)
-	})
-}
+let boot = function () {
+    server.listen(port, function () {
+        console.log('Express server listening on port ', port);
+    })
+};
 // shutdown server function
-let shutdown = function() {
-	server.close()
-}
+let shutdown = function () {
+    server.close()
+};
 
 // if main module then start server else pass to exports
-if(require.main === module){
-	boot()
+if (require.main === module) {
+    boot()
 } else {
-	console.log('Running socialAuth app as module')
-	module.exports = {
-		boot: boot,
-		shutdown: shutdown,
-		port: port,
-		server: server,
-		app: app
-	}
+    module.exports = {
+        boot:     boot,
+        shutdown: shutdown,
+        port:     port,
+        server:   server,
+        app:      app
+    }
 }
