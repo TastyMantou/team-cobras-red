6f88f2416e468744311cda6618d8a1228b7587ca
Pablo Lazo <plazo@usnews.com>
2016-06-29 11:38:43 -0400

    Set up vue and webpack

---
+++ client/src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="login">
+        <h1>Log In</h1>
+        <label>Email:<br>
+            <input type="text"/>
+        </label>
+        <label>Password:<br>
+            <input type="password"/>
+        </label>
+        <button>Submit</button>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+    h1 {
+        color: #42b983;
+    }
+    label {
+        display: block;
+        margin-bottom: .5rem;
+    }
+    .login {
+        text-align: left;
+    }
+</style>
