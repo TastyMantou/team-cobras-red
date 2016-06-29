a15a8c4f97c120787225498b1a5d085539ded9b6
Kevin Shreve <Kevin.Shreve@trinity-health.org>
2016-06-29 14:47:35 -0400

    not working 100%

--- client/src/components/Todo.vue
+++ client/src/components/Todo.vue
@@ -1,30 +1,25 @@
 <template>
-    <li v-bind:class="['todo', {'todo--active': selectedTodo && todo.id === selectedTodo.id}, {'todo--completed': todo.completed}]" v-on:click.self="selectTodo(todo)">
-        <input v-if="selectedTodo && selectedTodo.editing && selectedTodo.id === todo.id" v-model="selectedTodo.description" placeholder="todo.description"/>
-        <span v-on:click.self="editTodoText(todo)">{{todo.description}}</span>
-        <div v-bind:class="['todo__edit-options', {'todo__edit-options--active': selectedTodo && todo.id === selectedTodo.id}]">
-            <select v-model="todo.severity" v-if="!todo.completed">
-                <option v-for="option in severityOptions" v-bind:selected="option===todo.severity">{{option}}</option>
-            </select>
-            <span v-if="todo.completed">{{todo.severity}}</span>
-            <i class="ion-trash-b todo-list__action todo-list__action--delete" v-on:click.self="deleteTodo"></i>
-            <i class="ion-android-done-all todo-list__action todo-list__action--complete" v-on:click.self="completeTodo" v-if="selectedTodo && !selectedTodo.completed"></i>
-        </div>
-    </li>
+    <div>
+        <li v-bind:class="['todo', {'todo--active': selectedTodo && todo.id === selectedTodo.id}, {'todo--completed': todo.completed}]">
+            <input v-if="selectedTodo && selectedTodo.editing && selectedTodo.id === todo.id" v-model="todo.description" placeholder="*todo.description"/>
+            <span v-on:click.self="editTodoText(todo)">{{todo.description}}</span>
+            <div v-bind:class="['todo__edit-options', {'todo__edit-options--active': selectedTodo && todo.id === selectedTodo.id}]">
+                <select v-model="todo.severity" v-if="!todo.completed">
+                    <option v-for="option in severityOptions" v-bind:selected="option===todo.severity">{{option}}</option>
+                </select>
+                <span v-if="todo.completed">{{todo.severity}}</span>
+                <i class="ion-trash-b todo-list__action todo-list__action--delete" v-on:click.self="deleteTodo"></i>
+                <i class="ion-android-done-all todo-list__action todo-list__action--complete" v-on:click.self="completeTodo" v-if="selectedTodo && !selectedTodo.completed"></i>
+            </div>
+        </li>
+    </div>
 </template>
 
 <script>
-    function randomGuid () {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            let r = Math.random() * 16 | 0
-            let v = c === 'x' ? r : r & 0x3 | 0x8
-            return v.toString(16)
-        })
-    }
-
     export default {
         props: {
-            todo: Object
+            todo: Object,
+            selectedTodo: Object
         },
         data: function () {
             return {
@@ -36,44 +31,21 @@
             }
         },
         methods: {
-            selectTodo: function (todo) {
-                this.selectedTodo = (this.selectedTodo === todo) ? null : todo
-                return this.selectedTodo
-            },
             editTodoText: function (todo) {
+                let selectedTodo = this.selectedTodo
                 if (!todo.completed) {
-                    if (!this.selectedTodo || this.selectedTodo.id !== todo.id) {
-                        this.selectTodo(todo)
+                    if (!selectedTodo || selectedTodo.id !== todo.id) {
+                        selectedTodo = this.$parent.selectTodo(todo)
                     }
-                    this.selectedTodo = Object.assign({}, this.selectedTodo, {editing: !this.selectedTodo.editing})
+                    this.selectedTodo = Object.assign({}, selectedTodo, {editing: !selectedTodo.editing})
                     return this.selectedTodo
                 }
             },
             deleteTodo: function () {
-                this.todos = this.todos.filter((todo) => todo !== this.selectedTodo)
-                return this.todos
+                return this.$parent.deleteTodo()
             },
             completeTodo: function () {
-                let index = this.todos.findIndex((todo) => todo.id === this.selectedTodo.id)
-
-                this.todos[index] = Object.assign({}, this.selectedTodo, {completed: true})
-                this.selectTodo()
-                return this.todos
-            },
-            addTodo: function () {
-                let todo = {id: randomGuid(), description: 'New Todo', severity: 'low'}
-                this.todos.push(todo)
-                return this.selectTodo()
-            },
-            cancelTodo: function () {
-                return this.selectTodo()
-            },
-            saveTodo: function () {
-                let index = this.todos.findIndex((todo) => todo.id === this.selectedTodo.id)
-
-                this.todos[index] = this.selectedTodo
-                this.selectTodo()
-                return this.todos
+                return this.$parent.completeTodo()
             }
         }
     }
