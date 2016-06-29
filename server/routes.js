d1adb8f8c3ade06bbe064db6b7a986fd653e9262
Kevin Shreve <kshreve@gmail.com>
2016-06-28 22:23:47 -0400

    Adding gitignore and tasks route so far

--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const path = require('path')
-let db = require('./db')
+const path = require('path');
+let db = require('./db');
 
 module.exports = function (app, passport) {
 
     /******************************normal routes***************************************/
     // home page
     app.get('/', function (req, res) {
-    	res.sendFile('index.html', { root: './../' })
+        res.sendFile('index.html', { root: './../' });
     });
 
     // account data
@@ -18,60 +18,63 @@ module.exports = function (app, passport) {
 
     // tasks data
     app.get('/tasks', isLoggedIn, function (req, res) {
-        // get tasks from db with username info
+        res.sendFile('tasks.html', { root: './../' });
     });
 
     // logout data
     app.get('/logout', function (req, res) {
         req.logout();
-        return res.send({ message: 'User logged out.'} )
+        return res.send({ message: 'User logged out.' });
     });
 
-
     // still need routes for password change, task update, admin view, password reset request, password reset
-    
-
-
 
     // process a login
-    app.post('/login', function(req, res, next) {
-    	passport.authenticate('local-login', function (err, user, info) {
-    		if (err) return next(err)
+    app.post('/login', function (req, res, next) {
+        passport.authenticate('local-login', function (err, user, info) {
+            if (err) {
+                return next(err);
+            }
 
-	    	if (!user) return res.send(info)
+            if (!user) {
+                return res.send(info);
+            }
 
-			req.logIn(user, function(err) {
-	    		if (err) return next(err)
-	    		// need to send email and all tasks for logged in user - same as gettasks route but with user email too
-	    		return res.send(req.user.email)
-	    	})
-		})(req, res, next)
-	})
-
+            req.logIn(user, function (err) {
+                if (err) {
+                    return next(err);
+                }
+                // need to send email and all tasks for logged in user - same as gettasks route but with user email too
+                return res.send(req.user.email);
+            })
+        })(req, res, next);
+    });
 
     // process a sign up
-    app.post('/signup', function(req, res, next) {
-    	passport.authenticate('local-signup', function (err, user, info) {
-    		if (err) return next(err)
+    app.post('/signup', function (req, res, next) {
+        passport.authenticate('local-signup', function (err, user, info) {
+            if (err) {
+                return next(err);
+            }
 
-	    	if (!user) return res.send(info)
+            if (!user) {
+                return res.send(info);
+            }
 
-	    	req.logIn(user, function(err) {
-	    		if (err) return next(err)
+            req.logIn(user, function (err) {
+                if (err) {
+                    return next(err)
+                }
 
-	    		return res.send(req.user.email)
-	    	})
-		})(req, res, next)
-	})
+                return res.send(req.user.email)
+            });
+        })(req, res, next);
+    })
 };
-
-
-
-
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
     } // if authenticated then go to next middleware
-    res.json( {message: 'user not logged in. Please login'} ); // if not authenticated then redirect to home page
+    res.json({ message: 'user not logged in. Please login' }); // if not authenticated then redirect to home page
 }
